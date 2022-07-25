94f4695ccbbc700d1297ead4f9e902223a3e95d8
kim seongwon <seongwon1290@dsm.hs.kr>
2022-07-25 10:43:46 +0900

    🏋️‍♂️ :: 프로젝트 생성

---
+++ commit-health/src/main/java/com/example/commithealth/CommitHealthApplication.java
@@ -0,0 +1,13 @@
+package com.example.commithealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitHealthApplication.class, args);
+	}
+
+}
---
+++ commit-health/src/test/java/com/example/commithealth/CommitHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commithealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f52773deabc5d2a24030acaebb21d316a4b18165
kim seongwon <seongwon1290@dsm.hs.kr>
2022-07-28 00:28:22 +0900

    🔥 :: 안쓰는 임포트 삭제

--- commit-health/src/main/java/com/example/commithealth/global/config/SecurityConfig.java
+++ commit-health/src/main/java/com/example/commithealth/global/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 

7115c0e2247c1ee0dd94efd4c0a8afa6df6368e4
kim seongwon <seongwon1290@dsm.hs.kr>
2022-07-27 14:33:27 +0900

    ✨ :: 로그인 요청

--- commit-health/src/main/java/com/example/commithealth/domain/user/controller/UserController.java
+++ commit-health/src/main/java/com/example/commithealth/domain/user/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.commithealth.domain.user.controller;
 
+import com.example.commithealth.domain.user.controller.dto.request.LoginRequest;
 import com.example.commithealth.domain.user.controller.dto.request.SignupRequest;
 import com.example.commithealth.domain.user.service.UserService;
+import com.example.commithealth.global.security.jwt.TokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,5 +20,9 @@ public class UserController {
     @PostMapping("/sing-up")
     public void signup(@Valid SignupRequest request){
         service.signup(request);
+    }
+    @PostMapping("/login")
+    public TokenResponse login(@Valid LoginRequest request){
+        return service.login(request);
     }
 }

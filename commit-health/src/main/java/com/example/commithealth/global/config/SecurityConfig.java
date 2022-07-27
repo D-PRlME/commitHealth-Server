ce319c6aaf19d7d9bbb5dfde8684d2bd50260725
kim seongwon <seongwon1290@dsm.hs.kr>
2022-07-27 10:55:28 +0900

    🏋️‍♂️ :: SecurityConfig
    패스워드 인코더 추가

---
+++ commit-health/src/main/java/com/example/commithealth/global/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.commithealth.global.config;
+
+import com.example.commithealth.global.error.ExceptionFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}

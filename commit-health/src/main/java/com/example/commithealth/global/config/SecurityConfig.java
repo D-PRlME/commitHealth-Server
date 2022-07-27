ca2643665452ead5dd6abbbe55f914d8258a5505
kim seongwon <seongwon1290@dsm.hs.kr>
2022-07-27 14:25:33 +0900

    ✨ :: Jwt 등록, 허용 범위 추가

--- commit-health/src/main/java/com/example/commithealth/global/config/SecurityConfig.java
+++ commit-health/src/main/java/com/example/commithealth/global/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.commithealth.global.config;
 
 import com.example.commithealth.global.error.ExceptionFilter;
+import com.example.commithealth.global.security.jwt.JwtConfigurer;
+import com.example.commithealth.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +18,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ExceptionFilter exceptionFilter;
+
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
+    public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().disable()
+                .formLogin().disable()
+                .cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/user/sign-up").permitAll()
+                .antMatchers("/user/login").permitAll()
+                .anyRequest().authenticated()
+                .and().apply(new JwtConfigurer(jwtTokenProvider, exceptionFilter));
     }
 }
